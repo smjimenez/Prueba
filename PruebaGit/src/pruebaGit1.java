236f7614caab54b3afbd5059bf355ada4736d74b
smjimenez <smjimenez@106-03.info2>
2012-05-18 13:51:29 +0200

    rama3

--- PruebaGit/src/pruebaGit1.java
+++ PruebaGit/src/pruebaGit1.java
@@ -5,7 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Prueba 1");
 	System.out.println("Segundo commit");
 	
-	System.out.println("Empezamos rama nueva");
 	System.out.println("vamos a la rama dos");
 }
 }
